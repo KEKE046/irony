3b40782bde8d055d690a299b492d7450e41bff64
KEKE046 <KEKE_046@pku.edu.cn>
2023-07-24 09:26:36 +0800

    first init

---
+++ irony/src/fr/values.rs
@@ -0,0 +1,100 @@
+use std::{rc::Rc, slice, ops::{Deref, DerefMut}, hash::Hash};
+
+use super::types::TypeRef;
+
+pub struct Value<'ctx> {
+  tpe: TypeRef<'ctx>
+}
+
+#[derive(Clone,Copy)]
+pub struct ValueRef<'op, 'ctx>(&'op Value<'ctx>);
+impl<'op, 'ctx> ValueRef<'op, 'ctx> {
+  fn new(value: &'op Value<'ctx>) -> Self {Self(value)}
+}
+impl<'op, 'ctx> PartialEq for ValueRef<'op, 'ctx> {
+  fn eq(&self, other: &Self) -> bool { std::ptr::eq(self.0, other.0) }
+}
+impl<'op, 'ctx> Eq for ValueRef<'op, 'ctx> {}
+impl<'op, 'ctx> Deref for ValueRef<'op, 'ctx> {
+  type Target = Value<'ctx>;
+  fn deref(&self) -> &Self::Target {&self.0}
+}
+impl<'op, 'ctx> Hash for ValueRef<'op, 'ctx> {
+  fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+    std::ptr::hash(self.0, state)
+  }
+}
+
+pub fn type_iter<'ctx, I>(val_iter: I) -> impl Iterator<Item=TypeRef<'ctx>> 
+where I: Iterator<Item=&'ctx ValueRef<'ctx>> {
+  val_iter.map(|x|{x.tpe})
+}
+
+struct GenericOp<'ctx> {
+  operands: Vec<ValueRef<'ctx>>,
+  results: Vec<ValueRef<'ctx>>,
+}
+
+pub trait Op<'ctx> {
+  fn get_results(&'ctx self) -> impl Iterator<Item=&ValueRef<'ctx>>;
+  fn get_operands_mut(&'ctx mut self) -> impl Iterator<Item=&mut ValueRef<'ctx>>;
+  fn get_num_results(&'ctx self) -> usize {self.get_results().count()}
+  fn get_result_types(&'ctx self) -> impl Iterator<Item=TypeRef<'ctx>> { type_iter(self.get_results()) }
+  fn get_operands(&'ctx self) -> impl Iterator<Item=&ValueRef<'ctx>>;
+  fn get_num_operands(&'ctx self) -> usize {self.get_operands().count()}
+  fn get_operand_types(&'ctx self) -> impl Iterator<Item=TypeRef<'ctx>> { type_iter(self.get_operands()) }
+}
+
+macro_rules! variadic_op {
+  ($($name:ident),*) => {$(
+    struct $name<'ctx> {
+      operands: Vec<ValueRef<'ctx>>,
+      result: ValueRef<'ctx>
+    }
+    impl<'ctx> Op<'ctx> for $name<'ctx> {
+      fn get_results(&'ctx self) -> impl Iterator<Item=&ValueRef<'ctx>> {
+        slice::from_ref(&self.result).iter()
+      }
+      fn get_operands(&'ctx self) -> impl Iterator<Item=&ValueRef<'ctx>> {
+        self.operands.iter()
+      }
+      fn get_operands_mut(&'ctx mut self) -> impl Iterator<Item=&mut ValueRef<'ctx>> {
+        self.operands.iter_mut()
+      }
+    }
+  )*}
+}
+
+macro_rules! binary_op {
+  ($($name:ident),*) => {$(
+    struct $name<'ctx> {
+      operands: [ValueRef<'ctx>; 2],
+      result: ValueRef<'ctx>
+    }
+    impl<'ctx> Op<'ctx> for $name<'ctx> {
+      fn get_results(&'ctx self) -> impl Iterator<Item=&ValueRef<'ctx>> {
+        slice::from_ref(&self.result).iter()
+      }
+      fn get_operands(&'ctx self) -> impl Iterator<Item=&ValueRef<'ctx>> {
+        self.operands.iter()
+      }
+      fn get_operands_mut(&'ctx mut self) -> impl Iterator<Item=&mut ValueRef<'ctx>> {
+        self.operands.iter_mut()
+      }
+    }
+    impl<'ctx> $name<'ctx> {
+      pub fn get_lhs(&self) -> &ValueRef<'ctx> {&self.operands[0]}
+      pub fn get_lhs_mut(&mut self) -> &mut ValueRef<'ctx> {&mut self.operands[0]}
+      pub fn get_rhs(&self) -> &ValueRef<'ctx> {&self.operands[1]}
+      pub fn get_rhs_mut(&mut self) -> &mut ValueRef<'ctx> {&mut self.operands[1]}
+    }
+  )*}
+}
+
+variadic_op!(AddOp, MulOp, AndOp, XorOp, OrOp, ConcatOp);
+binary_op!(SubOp, DivOp, RemOp);
+
+struct ConstantOp<'ctx> {
+  data: i32,
+  result: ValueRef<'ctx>
+}
---
+++ irony/src/lib.rs
@@ -0,0 +1,20 @@
+#![feature(return_position_impl_trait_in_trait)]
+#![allow(unused)]
+
+mod fr;
+mod utils;
+
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
