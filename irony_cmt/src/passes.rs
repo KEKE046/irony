2c207d42886d67fbfb8aa290708553cb31e81658
Youwei Xiao <shallwexiao@gmail.com>
2023-09-30 16:20:04 +0800

    add RemoveEventPass (not tested)

--- irony_cmt/src/passes.rs
+++ irony_cmt/src/passes.rs
@@ -1,9 +1,11 @@
 use core::panic;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use irony::{Entity, Environ, Op, OpId, PassManagerTrait, PassTrait};
 
-use crate::{AttributeEnum, EntityEnum, OpEnum, StringAttr};
+use crate::{
+  AttributeEnum, EntityEnum, EventSignal, OpEnum, StringAttr, TmpSelect, TmpWhen,
+};
 
 #[derive(Debug, Clone)]
 pub struct RenamePass;
@@ -86,9 +88,9 @@ impl PassTrait<(), ()> for RenamePass {
 }
 
 #[derive(Debug, Clone)]
-pub struct MoveOutputToTailPass;
+pub struct ReorderPass;
 
-impl PassTrait<(), ()> for MoveOutputToTailPass {
+impl PassTrait<(), ()> for ReorderPass {
   type EntityT = EntityEnum;
   type OpT = OpEnum;
 
@@ -104,17 +106,139 @@ impl PassTrait<(), ()> for MoveOutputToTailPass {
   where E: Environ<EntityT = Self::EntityT, OpT = Self::OpT> {
     let region = env.get_op(op).get_regions()[0].1[0];
 
-    let mut included = env.get_region(region).op_children.to_owned();
-    if let Some(index) = included
-      .iter()
-      .position(|op| matches!(env.get_op(op.to_owned()), OpEnum::HwOutput(_)))
-    {
-      let x = included.remove(index);
-      included.push(x);
-    };
+    let included = env.get_region(region).op_children.to_owned();
+    let mut head = Vec::new();
+    let mut body = Vec::new();
+    let mut tail = Vec::new();
+
+    for op_id in included {
+      match env.get_op(op_id) {
+        OpEnum::HwInput(_)
+        | OpEnum::EventDef(_)
+        | OpEnum::EventPort(_)
+        | OpEnum::EventSignal(_) => {
+          head.push(op_id);
+        },
+        OpEnum::HwOutput(_) => {
+          tail.push(op_id);
+        },
+        _ => {
+          body.push(op_id);
+        },
+      }
+    }
+    env.get_region_entry(region).and_modify(|region| {
+      region.op_children =
+        head.into_iter().chain(body.into_iter()).chain(tail.into_iter()).collect();
+    });
+
+    Ok(())
+  }
+}
+
+#[derive(Debug, Clone)]
+pub struct RemoveEventPass;
+
+impl PassTrait<(), ()> for RemoveEventPass {
+  type EntityT = EntityEnum;
+  type OpT = OpEnum;
+
+  fn check_op<E>(&self, env: &E, op: OpId) -> bool
+  where E: Environ<EntityT = Self::EntityT, OpT = Self::OpT> {
+    match env.get_op(op) {
+      OpEnum::HwModule(_) => true,
+      _ => false,
+    }
+  }
+
+  fn run_raw<E>(&self, env: &mut E, op: OpId) -> Result<(), ()>
+  where E: Environ<EntityT = Self::EntityT, OpT = Self::OpT> {
+    let mut event_signal_mapping: HashMap<irony::EntityId, irony::EntityId> = HashMap::new();
+    let mut wire_guarded_table = HashMap::new();
+    let region = env.get_op(op).get_regions()[0].1[0];
+    let included = env.get_region(region).op_children.to_owned();
+    let mut new_included = Vec::new();
+
+    for op_id in included.iter() {
+      let op = env.get_op(op_id.to_owned()).to_owned();
+      match op {
+        OpEnum::EventSignal(EventSignal {
+          event: Some(event),
+          signal: Some(signal),
+          ..
+        }) => {
+          event_signal_mapping.insert(event.to_owned(), signal.to_owned());
+          // new_included.push(op_id.to_owned());
+        },
+        OpEnum::TmpWhen(TmpWhen { cond: Some(cond), body: Some(body), .. }) => {
+          let body = env.get_region(body.to_owned()).op_children.to_owned();
+          for op_id in body {
+            let defs = env
+              .get_op(op_id)
+              .get_defs()
+              .into_iter()
+              .flat_map(|(_, v)| v.into_iter().filter_map(|x| x.map(|x| x.to_owned())))
+              .collect::<Vec<_>>();
+            for def in defs {
+              wire_guarded_table.insert(def.to_owned(), cond.to_owned());
+            }
+            new_included.push(op_id.to_owned());
+            env.get_op_entry(op_id).and_modify(|op| {
+              op.set_parent(Some(region));
+            });
+          }
+        },
+        _ => {
+          new_included.push(op_id.to_owned());
+        },
+      }
+    }
+
+    for op_id in new_included.iter() {
+      let op = env.get_op(op_id.to_owned()).to_owned();
+      match op {
+        OpEnum::TmpSelect(TmpSelect { conds, values, .. }) => {
+          let mut new_conds = Vec::new();
+          for (old_cond, value) in conds.iter().zip(values.iter()) {
+            let value_id = value.unwrap().to_owned();
+            if let None = old_cond.to_owned() {
+              let cond_id = wire_guarded_table
+                .get(&value_id)
+                .expect("value to be selected must be guarded")
+                .to_owned();
+              let cond = env.get_entity(cond_id).to_owned();
+              let signal_id = match cond {
+                EntityEnum::IREvent(_) => event_signal_mapping
+                  .get(&cond_id)
+                  .expect("event must be mapped to signal")
+                  .to_owned(),
+                EntityEnum::IRWire(_) => cond_id,
+                _ => {
+                  panic!()
+                },
+              };
+              new_conds.push(Some(signal_id));
+            } else {
+              new_conds.push(old_cond.to_owned());
+            }
+          }
+          env.get_op_entry(op_id.to_owned()).and_modify(|op| {
+            match op {
+              OpEnum::TmpSelect(TmpSelect { conds, .. }) => {
+                *conds = new_conds;
+              },
+              _ => {
+                panic!()
+              },
+            }
+          });
+        },
+        _ => {},
+      }
+    }
 
     env.get_region_entry(region).and_modify(|region| {
-      region.op_children = included;
+      region.op_children = new_included;
     });
 
     Ok(())
@@ -124,7 +248,8 @@ impl PassTrait<(), ()> for MoveOutputToTailPass {
 #[derive(Debug, Clone)]
 pub enum PassEnum {
   RenamePass(RenamePass),
-  MoveOutputToTailPass(MoveOutputToTailPass),
+  ReorderPass(ReorderPass),
+  RemoveEventPass(RemoveEventPass),
 }
 
 impl PassTrait<(), ()> for PassEnum {
@@ -135,7 +260,8 @@ impl PassTrait<(), ()> for PassEnum {
   where E: Environ<EntityT = Self::EntityT, OpT = Self::OpT> {
     match self {
       PassEnum::RenamePass(pass) => pass.check_op(env, op_id),
-      PassEnum::MoveOutputToTailPass(pass) => pass.check_op(env, op_id),
+      PassEnum::ReorderPass(pass) => pass.check_op(env, op_id),
+      PassEnum::RemoveEventPass(pass) => pass.check_op(env, op_id),
     }
   }
 
@@ -143,7 +269,8 @@ impl PassTrait<(), ()> for PassEnum {
   where E: Environ<EntityT = Self::EntityT, OpT = Self::OpT> {
     match self {
       PassEnum::RenamePass(pass) => pass.run_raw(env, op_id),
-      PassEnum::MoveOutputToTailPass(pass) => pass.run_raw(env, op_id),
+      PassEnum::ReorderPass(pass) => pass.run_raw(env, op_id), 
+      PassEnum::RemoveEventPass(pass) => pass.run_raw(env, op_id),
     }
   }
 }
